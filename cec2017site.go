ca3ddaf9161b134ea2d4c499ae7afb25018197d1
Enoch Tsang <etsang1@hotmail.com>
2016-07-26 23:36:40 -0600

    created header with underline animation on nav-bar

--- cec2017site.go
+++ cec2017site.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-    "io"
+    "html/template"
     "log"
     "net/http"
+    "os"
+    "path"
+    "path/filepath"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, "hello, world!\n")
+func check(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+    t := template.Must(template.ParseFiles(path.Join(filepath.Dir(os.Args[0]), "templates/index.html")))
+    err := t.Execute(w, nil)
+    check(err)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, path.Join(filepath.Dir(os.Args[0]), "resources/images/favicon.ico"))
 }
 
 func main() {
-    http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/", home)
+    http.Handle("/resources/", http.StripPrefix("/resources/",
+        http.FileServer(http.Dir(path.Join(filepath.Dir(os.Args[0]), "resources")))))
+    // http.HandleFunc("/favicon.ico", faviconHandler)
+
     err := http.ListenAndServeTLS(":8443", "temp.crt", "temp.key", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
