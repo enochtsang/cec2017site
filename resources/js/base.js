599b7051034a5ec7c22b75befdc010c8c798902a
Enoch Tsang <etsang1@hotmail.com>
2016-08-06 22:03:59 -0600

    clean up style

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Fade in drop downs on nav-bar
-    $("#nav-bar li").hover(function() {
+    $(".nav-bar li").hover(function() {
         $(this).children(".nav-dropdown").fadeIn("fast");
     }, function() {
         $(this).children(".nav-dropdown").fadeOut("fast");
@@ -10,5 +10,5 @@ $(document).ready(function(){
     var loc = window.location.pathname;
     var dir = loc.substring(1, loc.length) + "/";
     dir = dir.substring(0, dir.indexOf("/"));
-    $("#nav-bar a[href=\"/" + dir + "\"").css("color", "red");
+    $(".nav-bar a[href=\"/" + dir + "\"").css("color", "red");
 });
