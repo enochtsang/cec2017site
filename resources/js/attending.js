1662f96ee27283d004b650a766e4f93cf5455d66
Enoch Tsang <etsang1@hotmail.com>
2016-08-31 01:04:20 -0600

    major improvements to mobile

--- resources/js/attending.js
+++ resources/js/attending.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
     $(".delegate-package-label").click(function() {
         $("#delegate-package-pdf").slideToggle();
+        $('html, body').animate({
+            scrollTop: $("#delegate-package-pdf").offset().top
+        }, "slow");
         if($(".delegate-package-label .dropdown-arrow").css("transform") == 'none'){
             $(".delegate-package-label .dropdown-arrow").css("transform", "rotate(90deg)");
         } else {
@@ -11,6 +14,9 @@ $(document).ready(function(){
 
     $(".official-rulebook-label").click(function() {
         $("#official-rulebook-pdf").slideToggle();
+        $('html, body').animate({
+            scrollTop: $("#official-rulebook-pdf").offset().top
+        }, "slow");
         if($(".official-rulebook-label .dropdown-arrow").css("transform") == 'none'){
             $(".official-rulebook-label .dropdown-arrow").css("transform", "rotate(90deg)");
         } else {
--- resources/js/base.js
+++ resources/js/base.js
@@ -11,4 +11,13 @@ $(document).ready(function(){
     var dir = loc.substring(1, loc.length) + "/";
     dir = dir.substring(0, dir.indexOf("/"));
     $(".nav-bar a[href=\"/" + dir + "\"").css("color", "#F20");
+
+    $(".mobile-nav-controller").click(function() {
+        $(".mobile-nav-bar ul").slideToggle();
+        if($(".mobile-nav-controller img").css("transform") == 'none'){
+            $(".mobile-nav-controller img").css("transform", "rotate(180deg)");
+        } else {
+            $(".mobile-nav-controller img").css("transform", "");
+        }
+    });
 });
