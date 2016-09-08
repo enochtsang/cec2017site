62444ec0ea97fe18c88822afd5a01d9415e1b27b
Enoch Tsang <etsang1@hotmail.com>
2016-09-08 16:08:00 -0600

    added blurbs

--- home.go
+++ home.go
@@ -10,8 +10,6 @@ type CommitteeMember struct {
     Name        string
     Class       string
     Role        string
-    Phone       string
-    Email       string
     PicturePath string
     Info        string
 }
@@ -38,142 +36,72 @@ func home_committee(w http.ResponseWriter, r *http.Request) {
         {
             Name:        "Christine Cao",
             Class:       "christine-cao",
-            Role:        "Chair",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Role:        "Co-Chair",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        ` Christine is a 4th Year Geomatics Engineering Student with a Biomedical Engineering Minor on her internship term with a software engineering company, called INSERT NAME HERE. In previous years she has been actively involved in the UofC Engineering Students’ Society and the Geomatics Engineering Students’ Society.`,
         },
         {
-            Name:        "Enoch Tsang",
-            Class:       "enoch-tsang",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Kaylyn Schnell",
+            Class:       "kaylyn-schnell",
+            Role:        "Co-Chair",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Kaylyn is a 4th Year Chemical Engineering Student currently on her internship with Canadian Natural Resources. Previously Kaylyn has held positions on the University of Calgary Students’ Union and the UofC Engineering Students’ Society. She has also been involved in coordinating the FIRST Robotics Western Canada Regional Competition for the past several years.`,
         },
         {
-            Name:        "Byron Seto",
-            Class:       "byron-seto",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Arsalan Fardi",
+            Class:       "arsalan-fardi",
+            Role:        "VP Hospitality",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Arsalan is a 4th Year Chemical Engineering Student with a Petroleum Minor currently on his internship term with TransCanada. He is an active member of the Chemical Engineering Students’ Society and the Petroleum and Energy Society on campus.`,
         },
         {
-            Name:        "Lami Opadokun",
-            Class:       "lami-opadokun",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Leah Loeppky",
+            Class:       "leah-loeppky",
+            Role:        "VP Events",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Leah is a 3rd Year Mechanical Engineering Student and the current President of the Mechanical Engineering Students’ Society. She recently completed a summer work term in Lloydminster, Saskatchewan with Husky Energy.`,
         },
         {
-            Name:        "Henry Tran",
-            Class:       "henry-tran",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Cindy Cai",
+            Class:       "cindy-cai",
+            Role:        "Co-VP Sponsorship",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Cindy is a 3rd Year Chemical Engineering Student with a Petroleum Minor and spends her spare time volunteering as an Energy Engineering Instructor for Minds in Motion, helping to engage youth in STEM subjects.`,
         },
         {
-            Name:        "Shawn Ayers",
-            Class:       "shawn-ayers",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Manpreet Deol",
+            Class:       "manpreet-deol",
+            Role:        "Co-VP Sponsorship",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Manpreet is a 2nd Year Mechanical Engineering Student and also the current VP External of the Mechanical Engineering Students’ Society. At the University of Calgary she has been involved in the Sikh Students’ Association and the Engineering Students’ Society. Manpreet has won a variety of awards and scholarships including a 2015 Schulich Leader of Canada Scholarship and she has been named one of Calgary’s Top 5 Youth to Watch.`,
         },
         {
-            Name:        "David Gamba",
-            Class:       "david-gamba",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Jennifer Busser",
+            Class:       "jenniger-busser",
+            Role:        "VP Translations",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Jenn is a 4th Year Geomatics Engineering Student currently on her internship with NovAtel Inc. She has previously been involved in the Geomatics Engineering Students’ Society and the 2015 Alberta Student Energy Conference.`,
         },
         {
-            Name:        "Justin Franco",
-            Class:       "justin-franco",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Hugo Olaciregui",
+            Class:       "justin-olaciregui",
+            Role:        "VP Finance",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Hugo is a 4th Year Mechanical Engineering Student on his internship term with Crescent Point Energy. He has previously been involved in exchange programs at the Université de Montréal and University of Stuttgart in Germany. He is also bilingual, speaking both English and Spanish.`,
         },
         {
-            Name:        "Eunice Wong",
-            Class:       "eunice-wong",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Matthew Buchko",
+            Class:       "matthew-buchko",
+            Role:        "VP Logistics",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Matt is a 4th Year Chemical Engineering Student with a Petroleum Minor on his internship with Crescent Point Energy. He has previously been involved in the UofC Engineering Students’ Society as the VP External and the Chemical Engineering Students’ Society. Originally from Canmore, Matt enjoys escaping to the Rocky Mountains to enjoy hiking and photography. `,
         },
         {
-            Name:        "Sally Ingrid",
-            Class:       "sally-ingrid",
-            Role:        "Executive Manager",
-            Phone:       "403-123-4567",
-            Email:       "something@example.com",
+            Name:        "Jasmine Zhao",
+            Class:       "jasmine-zhao",
+            Role:        "VP Competitions",
             PicturePath: "/resources/images/placeholders/profile-placeholder.png",
-            Info: ` The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.
-                    The quick brown fox jumps over the lazy dog.`,
+            Info:        `Jasmine is a 4th Year Chemical Engineering Student on her internship with TransCanada. She has previously been involved with the Petroleum and Energy Society, the Chemical Engineering Students’ Society and the UofC Engineering Students’ Society.`,
         },
     }
     err := t.ExecuteTemplate(w, "base", committee)
